cde301005d64d7342ef56873a66534395ec9c7ae
Kurtis Young <kurtisyoung@Kurtis-MacBook-Pro.local>
2025-03-28 09:43:46 -0700

    fix: test deployment one more time

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [reactRouter(), tsconfigPaths(), vanillaExtractPlugin()],
-  // If the DEPLOY_ENV is production, the base is /react-task-manager/
-  base: process.env.DEPLOY_ENV === "production" ? "/react-task-manager/" : "/",
+  base: "",
 });
